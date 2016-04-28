1b24414032eee23e3197981ffdac78e66c92f22a
janhorvat <jan.horvat@gmail.com>
2016-04-28 13:45:28 +0200

    share on instagram

--- js/index.js
+++ js/index.js
@@ -38,29 +38,17 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
 
-        $("#share-button").click(function() {
-            console.log("share butto2n");
-                var options = {
-          message: 'share this', // not supported on some apps (Facebook, Instagram)
-          subject: 'the subject', // fi. for email
-          url: 'https://www.website.com/foo/#bar?a=b',
-          chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title
-        }
-
-        var onS = function(result) {
-          console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
-          console.log("Shared to app: " + result.app); // On Android result.app is currently empty. On iOS it's empty when sharing is cancelled (result.completed=false)
-        }
-
-        var onE = function(msg) {
-          console.log("Sharing failed with message: " + msg);
-        }
-
-        window.plugins.socialsharing.shareWithOptions(options, onS, onE);
-});
-
-
-
+        $("#share-instagram").click(function() {
+            window.plugins.socialsharing.shareViaInstagram(
+                'Message via Instagram',
+                'https://www.google.nl/images/srpr/logo4w.png',
+                function() {
+                    console.log('share ok')
+                }, function(errormsg){
+                    alert(errormsg)
+                }
+            );
+        });
 
         $("#take-a-photo").click(function() {
             navigator.camera.getPicture(
